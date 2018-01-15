79640c0b1c37bb5c5891b475f3c1cbf068d08f93
Oleksii <iordatiy1991@gmail.com>
2018-01-15 19:41:14 +0200

    add more port

---
+++ q/js/custom.js
@@ -0,0 +1,18 @@
+ window.onload = function() {
+ 	var flkty = new Flickity( '.main-carousel', {
+	  // options
+	  cellAlign: 'left',
+	  contain: true,
+	  pageDots: false,
+	  prevNextButtons: false
+	});
+
+	var flkty = new Flickity( '.main-carousel2', {
+	  // options
+	  cellAlign: 'left',
+	  contain: true,
+	  pageDots: false,
+	  prevNextButtons: false
+	});	
+ };
+
